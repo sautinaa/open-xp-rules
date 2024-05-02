b116f6e00e1797c67faa60082c23d496f041d243
Andru <andreykr17@yandex.ru>
2024-05-02 21:31:03 +0300

    Added norm EventID 17 for Microsoft-Windows-Kernel-EventTracing provider

---
+++ packages/system/normalization_formulas/Microsoft/Windows/eventlog/Common/Kernel-EventTracing/17_The_security_descriptor_for_session_has_been_updated/tests/norm_1.js
@@ -0,0 +1,20 @@
+{
+    "action": "modify",
+    "datafield1": "2558525824",
+    "event_src.category": "Host security",
+    "event_src.fqdn": "win10-work.stand2008.local",
+    "event_src.hostname": "win10-work",
+    "event_src.subsys": "Microsoft-Windows-Kernel-EventTracing/Analytic",
+    "event_src.title": "windows",
+    "event_src.vendor": "microsoft",
+    "id": "Microsoft_Windows_eventlog_Common_EventTracing_17_The security_descriptor_for_session_has_been_updated",
+    "importance": "info",
+    "msgid": "17",
+    "object": "logging",
+    "object.id": "{d2112be4-cd15-5a9c-e38f-080a207e08d5}",
+    "object.name": "eventlog-system",
+    "object.type": "session",
+    "status": "success",
+    "subject": "application",
+    "time": "2024-05-02T15:59:20.641Z"
+}
