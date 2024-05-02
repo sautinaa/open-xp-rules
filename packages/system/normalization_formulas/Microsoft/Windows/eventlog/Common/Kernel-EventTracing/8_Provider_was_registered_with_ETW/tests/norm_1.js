c46f8811bffb6104614cd38098f42bb6eef4c3b2
Andru <andreykr17@yandex.ru>
2024-05-02 18:40:31 +0300

    Added norm EventID 8 for Microsoft-Windows-Kernel-EventTracing provider

---
+++ packages/system/normalization_formulas/Microsoft/Windows/eventlog/Common/Kernel-EventTracing/8_Provider_was_registered_with_ETW/tests/norm_1.js
@@ -0,0 +1,18 @@
+{
+    "action": "create",
+    "event_src.category": "Host security",
+    "event_src.fqdn": "win10-work.stand2008.local",
+    "event_src.hostname": "win10-work",
+    "event_src.subsys": "Microsoft-Windows-Kernel-EventTracing/Analytic",
+    "event_src.title": "windows",
+    "event_src.vendor": "microsoft",
+    "id": "Microsoft_Windows_eventlog_Common_EventTracing_8_Provider_was_registered_with_ETW",
+    "importance": "info",
+    "msgid": "8",
+    "object": "logging",
+    "object.name": "{6bd96334-dc49-441a-b9c4-41425ba628d8}",
+    "object.type": "provider",
+    "status": "success",
+    "subject": "application",
+    "time": "2024-05-02T15:28:41.956Z"
+}
