28d5cc6b925e419822dfeaa349f23b38a17ac0a5
Andru <andreykr17@yandex.ru>
2024-06-26 18:43:51 +0300

    Added 5805_The_session_setup_from_the_computer_failed_to_authenticate norm

---
+++ packages/system/normalization_formulas/Microsoft/Windows/eventlog/Common/System/5805_The_session_setup_from_the_computer_failed_to_authenticate/tests/norm_1.js
@@ -0,0 +1,17 @@
+{
+    "action": "login",
+    "event_src.category": "AAA",
+    "event_src.fqdn": "01566s-win16-ir.threebeesco.com",
+    "event_src.hostname": "01566s-win16-ir",
+    "event_src.subsys": "System",
+    "event_src.title": "NETLOGON",
+    "event_src.vendor": "microsoft",
+    "id": "Microsoft_Windows_eventlog_Common_System_5805_The_session_setup_from_the_computer_failed_to_authenticate",
+    "importance": "medium",
+    "object": "system",
+    "status": "failure",
+    "subject": "account",
+    "subject.account.name": "01566s-win16-ir",
+    "subject.type": "host",
+    "time": "2020-09-15T19:28:49.359Z"
+}
